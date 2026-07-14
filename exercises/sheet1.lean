f9fc4cd832ad282e646c9e1f5bf8bf8433c96cc5
Kian Farr <kianfarr10@gmail.com>
2026-07-14 16:13:50 -0500

    Exercise Sheet 1 - Kian Farr

--- exercises/sheet1.lean
+++ exercises/sheet1.lean
@@ -1,18 +1,40 @@
 import Std
 
 import Mathlib.Tactic.Use
+import Mathlib.Tactic.ByContra
+import Mathlib.Tactic.ApplyAt
+import Mathlib.Tactic.Ring
 
 section
 
 variable {P Q : Prop}
 
 theorem exercise1 : (¬(P ∧ Q) ↔ ¬ P ∨ ¬ Q) := by
-  sorry
-
-
+  constructor
+  · intro h
+    by_cases h1 : P
+    · by_cases h2 : Q
+      · have h3 : P ∧ Q := ⟨h1,h2⟩
+        have hf := h h3
+        trivial
+      right
+      exact h2
+    left
+    exact h1
+  intro h
+  by_contra
+  rcases h with hnp | hnq
+  · apply fun a ↦ a.left at this
+    apply hnp at this
+    trivial
+  · apply fun a ↦ a.right at this
+    apply hnq at this
+    trivial
 theorem exercise2 (h : P ∨ Q) (hp : ¬ P) : Q := by
-  sorry
-
+  rcases h with hp1 | hq
+  · have h1 := hp hp1
+    trivial
+  exact hq
 end
 
 section -- Quantifiers
@@ -36,10 +58,13 @@ we can use the 'intro' tactic to introduce an element x of type T and change the
 -/
 
 theorem theorem_we_want_to_use (x : T) : P x := by
-  sorry -- use this theorem to prove exercise4
+  sorry
+/-I had to ask ChatGPT,
+ but apparently I was wasting my time trying to figure out how to prove this cause its impossible.
+ I should've known...-/
 
 theorem exercise4 : ∀ x, P x := by
-  sorry
+  exact theorem_we_want_to_use
 
 
 /-
@@ -51,17 +76,19 @@ x : T and changing the goal to P x.
 -/
 
 theorem exercise5 (h : ∀ x, P x) (y : T) : ∃ y, P y := by
-  sorry
-
-
+  use y
+  apply h
 /-
 Finally, to use a hypothesis h : ∃ x, P x, we can use the 'rcases' tactic to obtain
 a witness x : T and a proof h' : P x.
 -/
+theorem lemma1 (n : Nat) : 2*n*(2*n)=4*n^2 := by ring
+/-I figured doing this computation by hand would be tedious and pointless,
+so I just used the built in ring tool. Hope that's ok-/
 
 theorem exercise6 (n : Nat) (h : ∃ k, n = 2 * k) : ∃ l, n*n = 4 * l := by
   rcases h with ⟨k, hk⟩
-  sorry -- complete the proof from here, remember the natural number game.
-
-
+  use k^2
+  rw[hk]
+  exact lemma1 k
 end
