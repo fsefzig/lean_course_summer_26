a1648c5db499d97c3526ff0622ef64019f9d5137
Leonardo Deng <leomd@Leos-MacBook.local>
2026-07-16 00:34:09 -0400

    Leonardo Deng, exercise sheet 1

---
+++ TESTING THINGS OUT/Practice2.lean
@@ -0,0 +1,65 @@
+import Std
+import Mathlib.Tactic.ByContra
+
+section
+
+variable {T : Type} {P Q : T → Prop} {R : Prop}
+
+theorem qpractice1 (h : ∀ x, P x) (a b : T) : P a ∧ P b := by
+  constructor
+  · exact h a
+  · exact h b
+
+
+theorem qpractice2 (h1 : ∀ x, P x → Q x) (h2 : ∀ x, P x) (a : T) : Q a := by
+  apply h1 a
+  exact h2 a
+
+theorem qpractice3 (h : ∀ x, P x ∧ Q x) : ∀ x, P x := by
+  intro x --x becomes a fixed, but unknown (unspecified; it can be anything) number
+  --goal becomes just show P x
+  have hx := h x
+  exact hx.1
+
+theorem qpractice4 (h : ∀ x, P x) : ∀ x, P x ∨ Q x := by
+  intro x
+  left
+  exact h x
+
+theorem qpractice5 : ∀ x : T, x = x := by
+  intro x
+  rfl
+
+theorem qpractice6 (a : T) (h : P a) : ∃ x, P x := by
+  sorry
+
+theorem qpractice7 (h : ∃ x, P x ∧ Q x) : ∃ x, P x := by
+  sorry
+
+theorem qpractice8 (h1 : ∃ x, P x) (h2 : ∀ x, P x → Q x) : ∃ x, Q x := by
+  sorry
+
+theorem qpractice9 (h : ∃ x, P x) : ¬ (∀ x, ¬ P x) := by
+  sorry
+
+theorem qpractice10 (h : ¬ ∃ x, P x) : ∀ x, ¬ P x := by
+  sorry
+
+theorem qpractice11 (h : ∀ x, ¬ P x) : ¬ ∃ x, P x := by
+  sorry
+
+theorem qpractice12 (n : Nat) (h : ∃ k, n = k + k) : ∃ l, n + n = l + l + l + l := by
+  sorry
+
+theorem qpractice13 (h : ∀ x, P x → Q x) : (∃ x, P x) → (∃ x, Q x) := by
+  sorry
+
+end
+
+
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/leomdeng/MathInLean.git
+git push -u origin main
--- lecture-notes/lecture1/examples1.lean
+++ lecture-notes/lecture1/examples1.lean
@@ -1,41 +1,91 @@
-import Std
+import Std -- standard module/library
 
-#check Nat.add_zero
+--on the left hand of the screen is code
+--right hand shows what we're doing
 
-#check Nat.mul_succ
+#check Nat.add_zero  --objects
+
+#check Nat.mul_succ --aka, these are the peano axioms
 
 #check Nat.mul_zero
+
+--note that we dont need to acc write "#check"
+--i think?
+--its just for ourselves
+
+
+
+-- |- means the goal
+-- everything above it is what we can use
+-- (if you click on line 18 or 19)
 
 -- Example 1
 theorem example1 (n : Nat) : n * Nat.succ 0 = n := by
   rw [Nat.mul_succ]
+  -- replace the expression we had with what we got
+  -- so the goal changes, from "so n*Nat.succ 0 =n"
+  -- to "show n*0+n=n"
+
   rw[Nat.mul_zero]
+  -- goal changes again
+  -- replaces n*0 with 0
+  -- so goal now is "show 0+n=n"
+
   sorry
+  -- sorry means like if the proof is
+  -- too complicated or you dont know how
+  -- it accepts it as a proof
+  -- and doesnt show up an error (it shows
+  -- an error if you dont do it
+
 
 -- Example 2
 theorem example2 (n : Nat) : 0 + n = n := by
   induction n with
-  | zero =>
-      exact Nat.zero_add 0
+  | zero =>   --this is the base case
+      exact Nat.zero_add 0 --apply axiom
+      --QUESTION: what does exact and rw mean?
+
   | succ n inductionHypothesis =>
       rw [Nat.add_succ, inductionHypothesis]
-
+  -- inductive step
+  --
 
 -- Example 1 - finished
 theorem example1' (n : Nat) : n * Nat.succ 0 = n := by
   rw [Nat.mul_succ]
   rw[Nat.mul_zero]
   exact example2 n
+  --EXACT means we just plug back n into the
+  --now-proven example2
+  --exactly example 2 applied to n
+
+  --it ONLY works if the statement is exactly what we have
+  --mainly for readability
+
 
 -- Example 3
 theorem example3 (n :Nat) : ∀ m, n + m = m + n := by
   induction n with
   | zero =>
       intro m
+      --tell lean "this is supposed to hold for all m"
+      --so, we introduce the variable m
+
       rw [Nat.add_zero]
+      --RW means rewrite (replace the old goal with new)
+
       rw [example2 m]
+      --rewrite LHS using example 2, then this goal done
   | succ n inductionHypothesis =>
       intro m
       rw [Nat.succ_add]
+      --axiom 4
       rw [inductionHypothesis]
       rw [Nat.add_succ]
+      --Felix cheating here lmao
+      --because this acc isnt an axiom
+      --or defined at all based on our 7 axioms
+      --proof is left to the reader
+
+
