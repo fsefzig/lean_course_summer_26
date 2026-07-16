5c6f82628fe86f67e51e820b1a61b70074dea026
Anshul Arul <anshularul@anshuls-mini.lan>
2026-07-15 17:34:53 -0700

    excercise-1-anshul-arul

--- exercises/sheet1.lean
+++ exercises/sheet1.lean
@@ -1,17 +1,40 @@
 import Std
 
 import Mathlib.Tactic.Use
+import Mathlib.Tactic.NthRewrite
 
 section
 
 variable {P Q : Prop}
 
 theorem exercise1 : (¬(P ∧ Q) ↔ ¬ P ∨ ¬ Q) := by
-  sorry
+  constructor
+  · intro h
+    by_cases hP : P
+    · right
+      intro hQ
+      apply h
+      constructor
+      · exact hP
+      · exact hQ
+    · left
+      exact hP
+  · intro h
+    intro hPQ
+    cases h with
+    | inl hp =>
+        exact hp hPQ.left
+    | inr hq =>
+        exact hq hPQ.right
 
 
 theorem exercise2 (h : P ∨ Q) (hp : ¬ P) : Q := by
-  sorry
+  cases h with
+    | inl hp2 =>
+        contradiction
+    | inr hq =>
+        exact hq
+
 
 end
 
@@ -36,10 +59,14 @@ we can use the 'intro' tactic to introduce an element x of type T and change the
 -/
 
 theorem theorem_we_want_to_use (x : T) : P x := by
-  sorry -- use this theorem to prove exercise4
-
-theorem exercise4 : ∀ x, P x := by
+  apply exercise3
+  intro x
   sorry
+  /- not sure how to do this one -/
+  
+theorem exercise4 : ∀ x, P x := by
+  intro x
+  apply theorem_we_want_to_use
 
 
 /-
@@ -51,7 +78,8 @@ x : T and changing the goal to P x.
 -/
 
 theorem exercise5 (h : ∀ x, P x) (y : T) : ∃ y, P y := by
-  sorry
+  use y
+  apply theorem_we_want_to_use
 
 
 /-
@@ -59,9 +87,17 @@ Finally, to use a hypothesis h : ∃ x, P x, we can use the 'rcases' tactic to o
 a witness x : T and a proof h' : P x.
 -/
 
+theorem two_mul_two_eq_four : (2 : Nat) * 2 = 4 := by
+  rfl
+
 theorem exercise6 (n : Nat) (h : ∃ k, n = 2 * k) : ∃ l, n*n = 4 * l := by
   rcases h with ⟨k, hk⟩
-  sorry -- complete the proof from here, remember the natural number game.
-
-
+  use k * k
+  rw [hk]
+  rw[← Nat.mul_assoc]
+  rw [Nat.mul_comm 2 k]
+  rw [Nat.mul_assoc k 2 2]
+  rw [Nat.mul_comm k (2*2)]
+  rw [two_mul_two_eq_four]
+  rw [Nat.mul_assoc]
 end
