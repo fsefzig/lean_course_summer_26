4d1da8e7463e6ad7aecf8e5cced3cb74dceae0e2
Havin Cimen <havincimen@Havin-Air.hgw.local>
2026-07-16 18:18:44 +0300

    havin-cimen, exercise sheet 1

--- exercises/sheet1.lean
+++ exercises/sheet1.lean
@@ -1,17 +1,35 @@
 import Std
 
 import Mathlib.Tactic.Use
+import Mathlib
 
 section
 
 variable {P Q : Prop}
 
 theorem exercise1 : (¬(P ∧ Q) ↔ ¬ P ∨ ¬ Q) := by
-  sorry
-
+  constructor
+  · intro h
+    by_cases hp : P
+    · right
+      intro hq
+      apply h
+      constructor
+      · exact hp
+      · exact hq
+    · left
+      exact hp
+  · intro h
+    intro hpq
+    cases h with
+    | inl hp => exact hp hpq.left
+    | inr hq => exact hq hpq.right
 
 theorem exercise2 (h : P ∨ Q) (hp : ¬ P) : Q := by
-  sorry
+  rcases h with hp | hq
+  · contradiction
+  · exact hq
+
 
 end
 
@@ -39,7 +57,8 @@ theorem theorem_we_want_to_use (x : T) : P x := by
   sorry -- use this theorem to prove exercise4
 
 theorem exercise4 : ∀ x, P x := by
-  sorry
+  intro x
+  exact theorem_we_want_to_use x
 
 
 /-
@@ -51,7 +70,8 @@ x : T and changing the goal to P x.
 -/
 
 theorem exercise5 (h : ∀ x, P x) (y : T) : ∃ y, P y := by
-  sorry
+  use y
+  exact h y
 
 
 /-
@@ -61,7 +81,9 @@ a witness x : T and a proof h' : P x.
 
 theorem exercise6 (n : Nat) (h : ∃ k, n = 2 * k) : ∃ l, n*n = 4 * l := by
   rcases h with ⟨k, hk⟩
-  sorry -- complete the proof from here, remember the natural number game.
-
+  -- complete the proof from here, remember the natural number game.
+  use k * k
+  rw [hk]
+  ring
 
 end
