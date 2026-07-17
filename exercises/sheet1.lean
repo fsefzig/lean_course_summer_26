36826da2d1f5c6336fc30081bdc926f1adc0cfa1
Maria Baginskaya <mariabaginskaya@Marias-MacBook-Air.local>
2026-07-16 19:30:17 -0700

    Mаriа-Bаginskаyа, excercise sheet 1

--- exercises/sheet1.lean
+++ exercises/sheet1.lean
@@ -1,22 +1,20 @@
 import Std
-
 import Mathlib.Tactic.Use
-
 section
 
 variable {P Q : Prop}
 
 theorem exercise1 : (¬(P ∧ Q) ↔ ¬ P ∨ ¬ Q) := by
-  constructor
+  apply Iff.intro
   · intro h
     by_cases hp : P
-    · right
+    · apply Or.inr
       intro hq
       apply h
-      constructor
+      apply And.intro
       · exact hp
       · exact hq
-    · left
+    · apply Or.inl
       exact hp
   · intro h
     intro hpq
@@ -25,7 +23,6 @@ theorem exercise1 : (¬(P ∧ Q) ↔ ¬ P ∨ ¬ Q) := by
         exact hnp hpq.1
     | inr hnq =>
         exact hnq hpq.2
-
 
 theorem exercise2 (h : P ∨ Q) (hp : ¬ P) : Q := by
   cases h with
@@ -40,23 +37,19 @@ section -- Quantifiers
 
 variable {T : Type} {y₀ : T} {P : T → Prop}
 
-theorem exercise3 (h : ∀ x, P x) (x : T) : P x := by
+theorem exercise3 (h : ∀ x, P x ) (x : T) : P x := by
   exact h x
-
 
 theorem theorem_we_want_to_use (x : T) : P x := by
   sorry
-
 
 theorem exercise4 : ∀ x, P x := by
   intro x
   exact theorem_we_want_to_use x
 
-
 theorem exercise5 (h: ∀ x, P x) : ∃ y, P y := by
   use y₀
   exact h y₀
-
 
 theorem exercise6 (n : Nat) (h : ∃ k, n = 2 * k) : ∃ l, n*n = 4 * l := by
   rcases h with ⟨k, hk⟩
