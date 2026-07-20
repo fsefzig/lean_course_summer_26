ab62f32aa2f1f99043536d2fb217467449f8b92b
zeryacimenn <zeryacimen1@gmail.com>
2026-07-20 19:47:39 +0300

    Update sheet2.lean

--- exercises/sheet2.lean
+++ exercises/sheet2.lean
@@ -26,7 +26,8 @@ theorem exercise0 {d k n : ℕ} (hn : n ≠ 0) (hd : d ≠ 1) (h : n = d * k) : 
     k < 2 * k := by omega
     _ ≤ d * k := Nat.mul_le_mul_right k hd2
     _ = n := h.symm
-theorem exercise1 {d n : ℕ} (hd : d ≠ 1) (h : d ∣ n) : ¬ (d ∣ n + 1):= by
+
+theorem exercise1 {d n : ℕ} (hd : d ≠ 1) (h : d ∣ n) : ¬ (d ∣ n + 1) := by
   intro h1
   have h_diff : d ∣ 1 := by
     exact (Nat.dvd_add_right h).mp h1
@@ -55,7 +56,7 @@ section -- Finsets
 
 #check Finset ℕ -- the type of finite subsets of ℕ
 
-variable {α : Type} [DecidableEq α] -- we need to be able to decide equality of elements
+variable {α : Type} [DecidableEq α]
 
 #check Finset α -- the type of finite sets formed by terms of type α
 
@@ -65,18 +66,17 @@ This works similarly to induction over ℕ. Use #check to find out how it works.
 -/
 #check Finset.induction_on
 
--- We can sum over finite sets, using the ∑ notation.
 variable {I : Finset α} {f : α → ℕ}
 
+-- We can sum over finite sets, using the ∑ notation.
 #check ∑ i ∈ I, f i
 
-
--- Use what we learned to prove the following theorem.
 theorem exercise3 (d : ℕ) (h : ∀ x, d ∣ f x) : d ∣ ∑ i ∈ I, f i := by
   induction' I using Finset.induction_on with a s ha ih
   · simp
   · rw [Finset.sum_insert ha]
     exact dvd_add (h a) ih
+
 end
 
 /-
