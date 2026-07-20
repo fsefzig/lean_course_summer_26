c509a235883eca030631e6f3137069b60c2a4efa
Maria Baginskaya <mariabaginskaya@Marias-MacBook-Air.local>
2026-07-19 21:43:26 -0700

    Maria Baginskaya, excercise sheet 2

--- exercises/sheet1.lean
+++ exercises/sheet1.lean
@@ -1,5 +1,4 @@
 import Std
-
 import Mathlib.Tactic.Use
 
 section
@@ -7,11 +6,29 @@ section
 variable {P Q : Prop}
 
 theorem exercise1 : (¬(P ∧ Q) ↔ ¬ P ∨ ¬ Q) := by
-  sorry
-
+  apply Iff.intro
+  · intro h
+    by_cases hp : P
+    · apply Or.inr
+      intro hq
+      apply h
+      exact And.intro hp hq
+    · apply Or.inl
+      exact hp
+  · intro h
+    intro hpq
+    cases h with
+    | inl hnp =>
+        exact hnp hpq.left
+    | inr hnq =>
+        exact hnq hpq.right
 
 theorem exercise2 (h : P ∨ Q) (hp : ¬ P) : Q := by
-  sorry
+  cases h with
+  | inl hp' =>
+      contradiction
+  | inr hq =>
+      exact hq
 
 end
 
@@ -19,49 +36,32 @@ section -- Quantifiers
 
 variable {T : Type} {P : T → Prop}
 
-/-
-Recall a proof of a universally quantified statement ∀ x, P x
-is an object of the product type ∏ (x : T), P x. In other words, a proof of ∀ x, P x
-is a function that takes an arbitrary element x of type T and returns a proof of P x.
-Thus, we can apply h : ∀ x, P x to an arbitrary element x : T to obtain a proof of P x.
--/
-
 theorem exercise3 (h : ∀ x, P x) (x : T) : P x := by
   exact h x
 
-
-/-
-Whenever we want to prove a universally quantified statement ∀ x, P x,
-we can use the 'intro' tactic to introduce an element x of type T and change the goal to P x.
--/
-
 theorem theorem_we_want_to_use (x : T) : P x := by
-  sorry -- use this theorem to prove exercise4
+  sorry
 
 theorem exercise4 : ∀ x, P x := by
-  sorry
-
-
-/-
-Recall a proof of an existentially quantified statement ∃ x, P x
-is an object of the sum type ∑ (x : T), P x. In other words, a proof of ∃ x, P x is a pair (x, h)
-where x : T and h : P x.
-We can use the 'use x' tactic to prove an existentially quantified statement by providing a witness
-x : T and changing the goal to P x.
--/
+  intro x
+  exact theorem_we_want_to_use x
 
 theorem exercise5 (h : ∀ x, P x) (y : T) : ∃ y, P y := by
-  sorry
-
-
-/-
-Finally, to use a hypothesis h : ∃ x, P x, we can use the 'rcases' tactic to obtain
-a witness x : T and a proof h' : P x.
--/
+  use y
+  exact h y
 
 theorem exercise6 (n : Nat) (h : ∃ k, n = 2 * k) : ∃ l, n*n = 4 * l := by
   rcases h with ⟨k, hk⟩
-  sorry -- complete the proof from here, remember the natural number game.
-
+  use k * k
+  rw [hk]
+  rw [hk]
+  -- now: (2 * k) * (2 * k) = 4 * (k * k)
+  rw [Nat.mul_assoc]
+  rw [Nat.mul_assoc]
+  rw [Nat.mul_comm k 2]
+  rw [← Nat.mul_assoc]
+  rw [Nat.mul_comm k 2]
+  rw [Nat.mul_assoc]
+  rfl
 
 end
