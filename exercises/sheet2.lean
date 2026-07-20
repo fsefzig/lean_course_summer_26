963ad56458041373f56a7afb2b7b9399941176a7
hedy-tang <yuanyuantang@outlook.com>
2026-07-20 01:22:34 -0700

    Hedy Tang, exercise sheet 2

--- exercises/sheet2.lean
+++ exercises/sheet2.lean
@@ -12,13 +12,75 @@ Your first task is to prove lemmas 0-3 from the lecture notes.
 section -- More on divisiblity
 
 theorem exercise0 {d k n : ℕ} (hn : n ≠ 0) (hd : d ≠ 1) (h : n = d * k) : k < n := by
-  sorry
+  by_contra! hk_ge_n
+  /- Lemmas -/
+  have hk_ne_zero : k ≠ 0 := by
+    intro hk
+    subst k
+    simp only [mul_zero] at h
+    exact hn h
+  have hd_ne_zero : d ≠ 0 := by
+    intro hd0
+    subst d
+    simp only [zero_mul] at h
+    exact hn h
+  /- Main Theorem -/
+  have hk_pos : 0 < k := Nat.pos_of_ne_zero hk_ne_zero
+  have hd_gt_one : 1 < d := by
+    cases d with
+    | zero =>
+        exact False.elim (hd_ne_zero rfl)
+    | succ d =>
+        cases d with
+        | zero =>
+            exact False.elim (hd rfl)
+        | succ d =>
+            simp
+  have hk_lt_dk : k < d * k := by
+    exact (Nat.lt_mul_iff_one_lt_left hk_pos).2 hd_gt_one
+  have hn_lt_dk : n < d * k := lt_of_le_of_lt hk_ge_n hk_lt_dk
+  rw [← h] at hn_lt_dk
+  exact Nat.lt_irrefl n hn_lt_dk
 
 theorem exercise1 {d n : ℕ} (hd : d ≠ 1) (h : d ∣ n) : ¬ (d ∣ n + 1):= by
-  sorry
+  intro h2
+  have h_one : d ∣ 1 := by
+    have h_sub : d ∣ (n + 1) - n := by
+      exact Nat.dvd_sub h2 h
+    rw [Nat.add_sub_cancel_left] at h_sub
+    exact h_sub
+  have hd_one : d = 1 := by
+    exact Nat.eq_one_of_dvd_one h_one
+  exact hd hd_one
 
 theorem infinitely_many_primes : ∀ n : ℕ, ∃ p : ℕ, p.Prime ∧ p > n := by
-  sorry
+  intro n
+  let N := n.factorial + 1
+  let p := N.minFac
+/- Facts -/
+  have hN_ne_one : N ≠ 1 := by
+    have hfac_pos : 0 < n.factorial := Nat.factorial_pos n
+    have hN_gt_one : 1 < N := by
+      exact Nat.lt_add_of_pos_left hfac_pos
+    exact Nat.ne_of_gt hN_gt_one
+/- -/
+  have hp_prime : p.Prime := by
+    exact Nat.minFac_prime hN_ne_one
+/- -/
+  have hp_dvd_N : p ∣ N := by
+    exact Nat.minFac_dvd N
+/- Main Theorem -/
+  refine ⟨p, hp_prime, ?_⟩
+  by_contra! hp_le_n
+/- -/
+  have hp_dvd_factorial : p ∣ n.factorial := by
+    exact Nat.dvd_factorial hp_prime.pos hp_le_n
+/- -/
+  have hp_not_dvd_next : ¬ p ∣ n.factorial + 1 := by
+    exact exercise1 hp_prime.ne_one hp_dvd_factorial
+/- -/
+  apply hp_not_dvd_next
+  exact hp_dvd_N
 
 end
 
@@ -44,7 +106,11 @@ variable {I : Finset α} {f : α → ℕ}
 
 -- Use what we learned to prove the following theorem.
 theorem exercise3 (d : ℕ) (h : ∀ x, d ∣ f x) : d ∣ ∑ i ∈ I, f i := by
-  sorry
+  refine Finset.induction_on I ?_ ?_
+  · exact dvd_zero d
+  · intro a S ha ih
+    rw [Finset.sum_insert ha]
+    exact Nat.dvd_add (h a) ih
 end
 
 /-
