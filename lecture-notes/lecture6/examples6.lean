607e402ca559738e6838cc7bdcb36e199b73649d
fsefzig <felix.sefzig@uzh.ch>
2026-07-27 11:53:37 +0100

    comments

--- lecture-notes/lecture6/examples6.lean
+++ lecture-notes/lecture6/examples6.lean
@@ -4,7 +4,7 @@ import Mathlib.Topology.MetricSpace.Pseudo.Defs
 import Mathlib.Topology.Instances.Rat
 
 /-
-How to manage a zoo of types a.k.a. coercion.
+How to manage a zoo of types, a.k.a. coercions.
 -/
 
 section
@@ -31,15 +31,16 @@ noncomputable example : ℝ := f (x + 1/n)
 
 /-
 What happens in the last example:
-1) Since f expects a real number, lean knows to that the '+' is the one for real numbers,
-so it needs to convert both x and 1/n to real numbers.
-2) Lean knows how to convert rational numbers to real numbers, so it converts x to a real number.
-3) Since lean expects a real number as 1/n, it converts n to a real number,
-and then computes 1/n as a real number.
+1. Since `f` expects a real number, Lean knows that `+` is addition on real
+   numbers, so it needs to convert both `x` and `1 / n` to real numbers.
+2. Lean knows how to convert rational numbers to real numbers, so it converts
+   `x` to a real number.
+3. Since Lean expects `1 / n` to be a real number, it converts `n` to a real
+   number and then computes `1 / n` as a real number.
 -/
 
 /-
-Lean is really good a figuring out which coercion to use.
+Lean is really good at figuring out which coercion to use.
 -/
 variable (k : Fin n) (l : Fin m)
 
@@ -54,7 +55,8 @@ variable (k : Fin n) (l : Fin m)
 
 /-
 Sequences of rational and real numbers.
-A sequence of rational numbers is a function from natural numbers to rational numbers `x : ℕ → ℚ`.
+A sequence of rational numbers is a function from natural numbers to rational
+numbers: `x : ℕ → ℚ`.
 -/
 
 namespace MySequences
@@ -72,26 +74,69 @@ variable {a b : ℝ} {c d : ℚ}
 example : dist a b = |a - b| := by
   rfl
 
-class RatSeq where
+/-
+`structure RatSeq` declares a new type, `RatSeq`, whose terms contain one field:
+a function `x : ℕ → ℚ`. Thus, if `s : RatSeq`, then `s.x` is the rational
+sequence stored in `s`.
+-/
+structure RatSeq where
   x : ℕ → ℚ
 
+/-
+This instance tells Lean that a `RatSeq` may be used as a function `ℕ → ℚ`.
+Thus, `f n` uses the stored function `f.x`.
+-/
 instance : CoeFun RatSeq (fun _ => ℕ → ℚ) where
   coe f := f.x
 
-class RealSeq where
+/-
+Likewise, `RealSeq` is a type whose terms package functions `ℕ → ℝ`.
+Although `RatSeq` and `RealSeq` have the same shape, they are distinct types:
+one stores a rational-valued function and the other a real-valued function.
+-/
+structure RealSeq where
   x : ℕ → ℝ
 
 instance : CoeFun RealSeq (fun _ => ℕ → ℝ) where
   coe f := f.x
 
+/-
+We can convert a `RatSeq` to a `RealSeq`. It builds a `RealSeq` from a
+`RatSeq` by coercing each rational value `f.x n` to a real value.
+-/
 abbrev RatSeq.toRealSeq (f : RatSeq) : RealSeq where
   x n := (f.x n : ℝ)
 
+/-
+This instance tells Lean to insert `RatSeq.toRealSeq` when it has a `RatSeq`
+but the expected type is `RealSeq`.
+-/
 instance : Coe RatSeq RealSeq where coe x := x.toRealSeq
 
+-- The expected result type causes Lean to insert the registered coercion.
 example (x : RatSeq) : RealSeq := x
 
--- Cauchy sequences. Note the type coercion in the defn (ε, and n, m, N)!
+/-
+How to define a sequence of rational numbers.
+-/
+example : RatSeq where
+  x n := 1 / n
+
+example : RatSeq where
+  x := fun n ↦ 1 / n
+
+-- Direct wrapper
+example : RatSeq := ⟨fun n ↦ 1 / n⟩
+
+-- use _ to create skeleton
+example : RatSeq := _
+
+
+/-
+We are ready to define Cauchy sequences and limits of sequences.
+Note the type coercions in the definitions, including those for `ε`, `n`, `m`,
+and `N`.
+-/
 def isCauchy (x : RatSeq) := ∀ ε > 0, ∃ N, ∀ m≥ N, ∀ n≥ N, dist (x m) (x n) < ε
 
 def tends_to (x : RatSeq) (a : ℚ) := ∀ ε > 0, ∃ N, ∀ n≥ N, dist (x n) a < ε
@@ -100,9 +145,11 @@ def isCauchyReal (x : RealSeq) := ∀ ε > 0, ∃ N, ∀ m≥ N, ∀ n≥ N, dis
 
 def tends_toReal (x : RealSeq) (a : ℝ) := ∀ ε > 0, ∃ N, ∀ n≥ N, dist (x n) a < ε
 
--- We can evaluate tends_toReal on a sequence of rational numbers!
+
+-- We can evaluate `tends_toReal` on a sequence of rational numbers.
 example (x : RatSeq) (a : ℝ) : tends_toReal x a := by sorry
 
+-- Find the proof of this on the exercise sheet.
 lemma isCauchy_toReal (x : RatSeq) : isCauchy x → isCauchyReal x := by
   sorry
 
@@ -110,8 +157,11 @@ lemma isCauchy_toReal (x : RatSeq) : isCauchy x → isCauchyReal x := by
 theorem real_numbers_complete {x : RatSeq} (hx : isCauchy x) : ∃ a : ℝ, tends_toReal x a := by
   sorry
 
-/- Uniqueness of the limit of a sequence of real numbers follows from the following result.
-Two numbers are equal if their distance is less than any positive number. -/
+/-
+Uniqueness of the limit of a sequence of real numbers follows from the
+following result: two numbers are equal if their distance is less than any
+positive number.
+-/
 
 #check @eq_of_forall_dist_le _ _ a b
 
@@ -134,6 +184,24 @@ lemma tends_toReal_unique {x : RealSeq} {a b : ℝ} (hx : tends_toReal x a) (hy 
         exact add_lt_add (Metric.mem_ball'.mp (hN K (le_max_left N M))) hKb
     _ = ε := by
       rw[add_halves]
+
+/-
+Bonus: Finally we can use a structure to define the type of Cauchy sequences!
+-/
+
+structure RatCauchySeq where
+  x : RatSeq
+  isCauchy : isCauchy x
+
+example : RatCauchySeq ≃ {x : RatSeq | isCauchy x} := by
+  exact
+    { toFun := fun x ↦ ⟨x.x, x.isCauchy⟩
+      invFun := fun x ↦ ⟨x.1, x.2⟩
+      left_inv := fun _ ↦ rfl
+      right_inv := fun _ ↦ rfl }
+
+--By using a structure instead of a subtype we get direct access to the fields of the structure.
+example (x : RatCauchySeq) : isCauchy x.x := x.isCauchy
 
 end MySequences
 
