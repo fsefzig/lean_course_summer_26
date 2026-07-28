4a2bd164be755f3c11bd41180075f6dc590b66f7
Grant Yang <rotartsi0482@gmail.com>
2026-07-28 11:51:25 -0700

    save

--- exercises/sheet5.lean
+++ exercises/sheet5.lean
@@ -1,4 +1,5 @@
 import LectureNotes.lecture6.examples6
+import Mathlib.Topology.MetricSpace.IsometricSMul
 
 open MySequences
 
@@ -16,10 +17,20 @@ example (x : ℝ) : ⌈x⌉ ≥ x := by exact Int.le_ceil x
 theorem exercise1 {ε : ℝ} (hε : ε > 0) : ∃ δ : ℕ , δ > 0 ∧ (1 / δ) ≤ ε := by
   by_cases h : ε ≤ 1
   · use ⌈1/ε⌉.natAbs
-    simp?
+    simp only [one_div, gt_iff_lt, Int.natAbs_pos, ne_eq, Int.ceil_eq_zero_iff, Set.mem_Ioc,
+      inv_nonpos, not_and, not_le, Nat.cast_natAbs, Int.cast_abs]
     refine ⟨fun x => hε, ?_⟩
-    repeat rw [inv_eq_one_div]
-    sorry
+    rw [← Int.cast_abs]
+    rw [inv_eq_one_div]
+    have hnonneg : 0 ≤ ⌈1/ε⌉ := by
+      apply Int.ceil_nonneg ?_
+      simp [le_of_lt hε]
+    apply one_div_le hε ?_ |>.mp
+    · rw [← one_div, abs_of_nonneg hnonneg]
+      apply Int.le_ceil (1/ε)
+    · rw [← one_div, abs_of_nonneg hnonneg, Int.cast_pos]
+      simp only [one_div, Int.ceil_pos, inv_pos]
+      exact hε
   push Not at h
   use 1
   simp [lt_iff_le_and_ne.mp h |>.1]
@@ -38,21 +49,57 @@ Hint below:
 #check Rat.dist_cast
 
 theorem exercise3 {x : RatSeq} : isCauchy x ↔ isCauchyReal x := by
-  sorry
-
+  constructor <;>
+  · unfold isCauchy isCauchyReal RatSeq.toRealSeq
+    simp [Rat.dist_cast]
 
 /-
 Finally, show that convergent sequences are Cauchy sequences.
 -/
 theorem exercise4 {x : RealSeq} (a : ℝ) (hx : tends_toReal x a) : isCauchyReal x := by
-  sorry
+  unfold tends_toReal at hx
+  unfold isCauchyReal
+  intro ε hε
+  obtain ⟨N, hN⟩ := hx ε hε
+  use N
+  intro m hm n hn
+  obtain h := hN m hm
+  obtain h' := hN n hn
+
+
+theorem fiveadd : (5 : ℝ) = @Nat.cast ℝ Real.instNatCast 5 := by
+  exact Eq.symm (Real.ext_cauchy rfl)
+
+#check dist_add_left
 
 /-
 Finally, define a sequence of real numbers that does not converge.
 -/
 
 def my_diverging_sequence : RealSeq where
-  x n := sorry
+  x n := n
+
 
 theorem exercise5 : ¬ ∃ a : ℝ, tends_toReal my_diverging_sequence a := by
+  unfold tends_toReal my_diverging_sequence
+  dsimp
+  intro ⟨a, ha⟩
+  obtain ⟨N, hN⟩ := ha 1 (by simp)
+  obtain h₁ := hN (N+1) (by simp)
+  obtain h₂ := hN (N+5) (by simp)
+  rw [Nat.cast_add] at h₁ h₂
+  rw [← fiveadd] at h₂
+  rw [Nat.cast_one] at h₁
+  have add := add_lt_add_of_lt_of_le h₁ (le_of_lt h₂)
+  have tri := dist_triangle (N + 1 : ℝ) a (N + 5)
+  rw [dist_comm a] at tri
+  have x := lt_of_le_of_lt tri add
+  have : IsIsometricVAdd ℝ ℝ := sorry
+  rw [dist_add_left] at x
+  have rwh : dist (1 : ℝ) 5 = 4 := by
+    sorry
+  rw [rwh] at x
+  rw [show (1 : ℝ) + 1 = 2 by sorry] at x
   sorry
+
+#check dist
