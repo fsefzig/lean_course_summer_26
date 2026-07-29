65a5f70c3efeed675415c7063d1a1a8818f89332
harshiln14 <hnukala14@gmail.com>
2026-07-29 08:40:38 +0530

    Harshil Nukala, exercise sheet 5

--- exercises/sheet5.lean
+++ exercises/sheet5.lean
@@ -14,15 +14,35 @@ example (x : ℝ) : ⌈x⌉ ≥ x := by exact Int.le_ceil x
 
 theorem exercise1 {ε : ℝ} (hε : ε > 0) : ∃ δ : ℕ , δ > 0 ∧ (1 / δ) ≤ ε := by
   by_cases h : ε ≤ 1
-  · sorry
-  sorry
+  · use (Nat.ceil (1/ε))
+    constructor
+    · simp
+      linarith
+    rw[one_div_le]
+    exact (Nat.le_ceil (1/ε))
+    positivity
+    exact hε
+  push Not at h
+  use 1
+  constructor
+  · norm_num
+  simp only [Nat.cast_one, ne_eq, one_ne_zero, not_false_eq_true, div_self]
+  exact le_of_lt h
 
+-- Sahasra says 'hi!'
 /-
 Show that convergence can be expressed in terms of rational numbers. Use the above exercise.
 -/
+
 theorem exericse2 {x : RealSeq} (a : ℝ) (hx : ∀ δ : ℕ, δ > 0 → ∃ N, ∀ n≥ N, dist (x n) a < 1 / δ)
   : tends_toReal x a := by
-  sorry
+  intro ε hε
+  obtain ⟨δ, δ_ge_zero, δ_le_ε⟩ := exercise1 hε
+  obtain ⟨N, hN⟩ := hx δ δ_ge_zero
+  use N
+  exact fun n a_1 ↦ Std.lt_of_lt_of_le (hN n a_1) δ_le_ε
+
+  -- Note: the last line was written by using _apply ?_
 
 /-
 Show that rational Cauchy sequences are also Cauchy sequences of real numbers and vice versa.
@@ -31,21 +51,54 @@ Hint below:
 #check Rat.dist_cast
 
 theorem exercise3 {x : RatSeq} : isCauchy x ↔ isCauchyReal x := by
-  sorry
-
+  constructor
+  · intro h
+    unfold isCauchy at h
+    unfold isCauchyReal
+    intro ε hε
+    obtain ⟨N,hN⟩ := h ε hε
+    use N
+    intro m hm n hn
+    rw[Rat.dist_cast (x.x m) (x.x n)]
+    exact Metric.mem_ball.mp (hN m hm n hn) -- used _exact?_
+  intro h
+  unfold isCauchyReal at h
+  unfold isCauchy
+  intro ε hε
+  obtain ⟨N, hN⟩ := h ε hε
+  use N
+  intro m hm n hn
+  rw[← Rat.dist_cast]
+  exact Metric.mem_ball.mp (hN m hm n hn)
 
 /-
 Finally, show that convergent sequences are Cauchy sequences.
 -/
 theorem exercise4 {x : RealSeq} (a : ℝ) (hx : tends_toReal x a) : isCauchyReal x := by
-  sorry
+  unfold isCauchyReal
+  unfold tends_toReal at hx
+  intro ε hε
+  have ⟨N, hN⟩ := hx (ε/2) (half_pos hε)
+  use N
+  intro m hm n hn
+  have h1 : dist (x.x n) a < ε / 2 := hN n hn
+  have h2 : dist (x.x m) a < ε / 2 := hN m hm
+  rw[dist_comm (x.x n) a] at h1
+  have ha : (dist (x.x m) a) + (dist a (x.x n)) < ε := by linarith
+  have ht := dist_triangle (x.x m) a (x.x n)
+  have hf : dist (x.x m) (x.x n) ≤ ε := by linarith
+  exact Std.lt_of_le_of_lt ht ha
 
 /-
 Finally, define a sequence of real numbers that does not converge.
 -/
 
 def my_diverging_sequence : RealSeq where
-  x n := sorry
+  x n := n
 
 theorem exercise5 : ¬ ∃ a : ℝ, tends_toReal my_diverging_sequence a := by
-  sorry
+  intro h
+  unfold tends_toReal at h
+  obtain ⟨a, ha⟩ := h
+  obtain ⟨N, hN⟩ := ha (1) (zero_lt_one)
+  set n := N + Nat.ceil(a + 1 : ℝ)
