b321ebe0151f80c97f450adde3ba3bcbb3f95cc0
fsefzig <felix.sefzig@uzh.ch>
2026-07-31 18:52:55 +0100

    fix solution5

--- exercises/sheet5.lean
+++ exercises/sheet5.lean
@@ -18,7 +18,7 @@ theorem exercise1 {ε : ℝ} (hε : ε > 0) : ∃ δ : ℕ , δ > 0 ∧ (1 / δ)
     refine ⟨Nat.ceil_pos.mpr (show 0 < ⌈1 / ε⌉ by positivity), ?_⟩
     apply (one_div_le _ _).mpr
     · have h : (⌈1 / ε⌉.toNat : ℝ) = ⌈1 / ε⌉ := by
-        exact_mod_cast Int.toNat_of_nonneg (show 0 ≤ ⌈1 / ε⌉ by positivity)
+        exact_mod_cast Int.toNat_of_nonneg (by positivity)
       rw[h]
       exact Int.le_ceil (1 / ε)
     · apply Nat.cast_pos'.mpr
@@ -64,11 +64,10 @@ Finally, show that convergent sequences are Cauchy sequences.
 -/
 theorem exercise4 {x : RealSeq} (a : ℝ) (hx : tends_to x a) : isCauchyReal x := by
   intro ε hε
-  have hε2 : ε / 2 > 0 := by positivity
-  obtain ⟨N, hN⟩ := hx (ε / 2) hε2
+  obtain ⟨N, hN⟩ := hx (ε / 2) (by positivity)
   use N
   intro m hm n hn
-  calc _
+  calc
     dist (x m) (x n) ≤ dist (x m) a + dist a (x n) := dist_triangle (x m) a (x n)
     _ = dist (x m) a + dist (x n) a := by rw[dist_comm a (x n)]
     _ < ε / 2 + ε / 2 := by exact add_lt_add (hN m hm) (hN n hn)
@@ -85,25 +84,18 @@ theorem exercise5 : ¬ ∃ a : ℝ, tends_to my_diverging_sequence a := by
   let x := my_diverging_sequence
   intro h
   obtain ⟨a, ha⟩ := h
-  have h1 := ha 1 Real.zero_lt_one
-  have h2 := ha 1 Real.zero_lt_one
-  obtain ⟨N, hN⟩ := h1
-  obtain ⟨M, hM⟩ := h2
-  let K := max N M
-  have hcon :  dist (x K) (x (K+1)) = 2 := by
+  have ⟨N, hN⟩ := ha 1 Real.zero_lt_one
+  have hcon :  dist (x N) (x (N+1)) = 2 := by
     calc
-      dist (x K) (x (K+1)) = abs ((-1 : ℝ) ^ K - (-1 : ℝ) ^ (K + 1)) := by rfl
-      _ = abs ((-1 : ℝ) ^ K * 2) := by ring_nf
+      dist (x N) (x (N+1)) = abs ((-1 : ℝ) ^ N - (-1 : ℝ) ^ (N + 1)) := by rfl
+      _ = abs ((-1 : ℝ) ^ N * 2) := by ring_nf
       _ = 1 * 2 := by simp only [abs_mul, abs_pow, abs_neg, abs_one, one_pow, Nat.abs_ofNat,
         one_mul]
       _ = 2 := by exact one_mul 2
-  have hkM : K ≥ M := by exact le_max_right N M
-  specialize hN K (le_max_left N M)
-  specialize hM (K + 1) (by linarith)
-  have hdist : dist (x K) (x (K + 1)) < 2 := by
+  have hdist : dist (x N) (x (N + 1)) < 2 := by
     calc
-      dist (x K) (x (K + 1)) ≤ dist (x K) a + dist a (x (K + 1)) := dist_triangle (x K) a (x (K + 1))
-      _ = dist (x K) a + dist (x (K + 1)) a := by rw[dist_comm a (x (K + 1))]
-      _ < 1 + 1 := by exact add_lt_add hN hM
+      dist (x N) (x (N + 1)) ≤ dist (x N) a + dist a (x (N + 1)) := dist_triangle (x N) a (x (N + 1))
+      _ = dist (x N) a + dist (x (N + 1)) a := by rw[dist_comm a (x (N + 1))]
+      _ < 1 + 1 := by exact add_lt_add (hN N (by linarith)) (hN (N + 1) (by linarith))
       _ = 2 := by exact one_add_one_eq_two
   linarith
