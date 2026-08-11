0394ff40c29025b572511d7438e71c3d919c8d83
harshiln14 <hnukala14@gmail.com>
2026-08-11 10:40:18 -0700

    setup

---
+++ LectureNotes/project_pi/project.lean
@@ -0,0 +1,21 @@
+
+/-
+Definitions:
+1.) Pi
+
+Theorems/Results:
+
+Known results:
+1.) Product rule for derivatives
+
+Unknown results:
+1.) Integral is monotone
+
+Proof Structure
+  Step 1: Define the functions f, F.
+  Stem 2: ...
+
+
+
+
+-/
